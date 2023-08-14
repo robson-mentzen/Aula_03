a3b82748d023682d57c351bdfb588ec626a310d7
robson.mentzen <robson.mentzen@LAB9-26-WIN.acad.univates>
2023-08-14 20:40:12 -0300

    Tela que grava a informação em outra tela.

--- Aula_03/src/TelaPrincipal.java
+++ Aula_03/src/TelaPrincipal.java
@@ -83,6 +83,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         String x = txtMessage.getText();
